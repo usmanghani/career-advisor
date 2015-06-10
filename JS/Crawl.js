323d6e0f35c7b95e6c0bb35f0b33971247d56ea8
Syed Hussain Mehdi <i.mehdi@pilabs.com>
2015-06-10 13:31:40 +0500

    Intelligent crawler for other /dir/ links

--- JS/Crawl.js
+++ JS/Crawl.js
@@ -14,8 +14,7 @@ var CrawlerTmp=  'crawl_'+system.args[1]+'.tmp';
 
 var url = "";
 
-//while (true)
-{
+
 
 if (!fs.exists(CrawlerDB))
   url    = 'https://www.linkedin.com/directory/people-'+system.args[1];
@@ -34,67 +33,127 @@ else
 	fs.remove(CrawlerTmp);
 }
 
+
+
 console.log("Crawling : "+url);
 
 
-page.open(url, function (status) {
-  console.log("Status: " + status);
 
-  if(status === "success") {
-	var link = page.evaluate(function() {
-    
-	//section last
-	var section = document.getElementsByClassName("section last")[0];
-	
-	if (section){
-		var columns = section.getElementsByClassName("columns")[0];
+
+
+if (url.indexOf("/directory/people")>0)
+{	
+	// /directory/people crawler
+	console.log("/directory/people crawler working\n");
+
+	page.open(url, function (status) {
+	  console.log("Status: " + status);
+
+	  if(status === "success") {
+		var link = page.evaluate(function() {
 		
-		// All links
-		var atags   = section.getElementsByTagName("a");
-		var allLinks = [];
-		for (var i=0; i<atags.length; i++)
-			allLinks[i]=atags[i].href;
-		return allLinks;
-		}
+		//section last
+		var section = document.getElementsByClassName("section last")[0];
 		
-	else{
-		Error = true;
-		return "Error : Possible IP ban or wrong link";
-		}
-    });
-	
-	if (!Error)
-	{
-	
-	 for (var i=0; i<link.length; i++)
-	  {
-		 if (link[i].indexOf("/directory/people")>0)
-		 {
-			 // Expanding
-			 fs.write(CrawlerDB, link[i]+"\r\n", 'a');
+		if (section){
+			var columns = section.getElementsByClassName("columns")[0];
 			
-		 }
-		 else if (link[i].indexOf("/dir/")>0)
-		 {
-			 // dir with same names
-			 fs.write(DirDB, link[i]+"\r\n", 'a');
-		 }
-		 else
-		 {
-			//processed 
-			fs.write(Processed, link[i]+"\r\n", 'a');
-		 }
-	  }	
-	}
-	
-	else 
-		console.log(link);
-	//console.log(link[i].indexOf("/directory/people"));  
-	}
-	
-	
+			// All links
+			var atags   = section.getElementsByTagName("a");
+			var allLinks = [];
+			for (var i=0; i<atags.length; i++)
+				allLinks[i]=atags[i].href;
+			return allLinks;
+			}
+			
+		else{
+			Error = true;
+			return "Error : Possible IP ban or wrong link";
+			}
+		});
+		
+		if (!Error)
+		{
+		
+		 for (var i=0; i<link.length; i++)
+		  {
+			 if (link[i].indexOf("/directory/people")>0)
+			 {
+				 // Expanding
+				 fs.write(CrawlerDB, link[i]+"\r\n", 'a');
+				
+			 }
+			 else if (link[i].indexOf("/dir/")>0)
+			 {
+				 // dir with same names
+				// fs.write(DirDB, link[i]+"\r\n", 'a');
+				fs.write(CrawlerDB, link[i]+"\r\n", 'a');
+			 }
+			 else
+			 {
+				//processed 
+				fs.write(Processed, link[i]+"\r\n", 'a');
+			 }
+		  }	
+		}
+		
+		else 
+			console.log(link); 
+		}
+		
+		
 
-phantom.exit();  
-});
-
+	phantom.exit();  
+	});	
 }
+
+else if (url.indexOf("/dir/")>0)
+{
+	// /dir/ crawler
+	console.log("/dir/ crawler working\n");
+	
+	page.open(url, function (status) {
+	  console.log("Status: " + status);
+
+	  if(status === "success") {
+		var links = page.evaluate(function() {
+		
+		//content-wrapper
+		var con_wrapper = document.getElementsByClassName("content-wrapper")[0];
+		
+		if (con_wrapper){
+			var photos = con_wrapper.getElementsByClassName("photos")[0];
+			
+			// All links
+			var atags   = photos.getElementsByTagName("a");
+			var allLinks = [];
+			for (var i=0; i<atags.length; i++)
+			allLinks[i]=atags[i].href;
+			
+			return allLinks;
+			}
+			
+		else{
+			Error=true;
+			return "Error : Possible IP ban or wrong link";
+			}
+		});
+		
+		if (!Error)
+		  for (var i=0; i<link.length; i++)
+		     fs.write(Processed, link[i]+"\r\n", 'a');
+		
+		else
+		   console.log(link);
+		}
+		
+		
+
+	phantom.exit();  
+	});	
+	
+	
+	
+	
+}
+
